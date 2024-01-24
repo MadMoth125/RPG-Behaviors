5c6c5ad2ebd85e9d91d48e3cad3dba1371e3acb1
MadMoth125 <madmoth125@gmail.com>
2024-01-24 14:24:51 -0600

    added interfaces

---
+++ RPG-Characters/RPG-Characters/Interfaces/IAttack.cs
@@ -0,0 +1,13 @@
+﻿
+namespace RPG_Characters.Interfaces
+{
+	public interface IAttack
+	{
+		/// <summary>
+		/// Interface for attacking a target.
+		/// Logic for the attack is handled in the class implementing this interface.
+		/// </summary>
+		/// <param name="target">The target being attacked.</param>
+		public void Attack(object target);
+	}
+}
---
+++ RPG-Characters/RPG-Characters/Interfaces/IDefend.cs
@@ -0,0 +1,14 @@
+﻿
+namespace RPG_Characters.Interfaces
+{
+	public interface IDefend
+	{
+		/// <summary>
+		/// Interface for defending against an attack.
+		/// Carries data about the instigator initiating the attack and the damage it is dealing.
+		/// </summary>
+		/// <param name="instigator">The instigator initiating the attack.</param>
+		/// <param name="damage">The damage value being dealt.</param>
+		public void Defend(object instigator, int damage);
+	}
+}
