508555beccc330f606f7db738448bab3cca8adc5
MadMoth125 <madmoth125@gmail.com>
2024-01-24 15:34:35 -0600

    Update Mage.cs
    changed how the mage attacks by adding more "spells" for them to cast.
    Added healing as well (idk if it works)

--- RPG-Characters/RPG-Characters/Classes/Mage.cs
+++ RPG-Characters/RPG-Characters/Classes/Mage.cs
@@ -7,8 +7,14 @@
 			MagicDamage = magicDamage;
 		}
 
-		public int MagicDamage { get; private set; }
+		public int MagicDamage
+		{
+			get => _magicDamage;
+			private set => _magicDamage = Math.Max(0, value);
+		}
 
+		private int _magicDamage;
+		
 		public override void Attack(object target)
 		{
 			// check if the target is a character
@@ -22,12 +28,18 @@
 					return;
 				}
 				
-				// announce the attack
-				Console.WriteLine($"{Name} casted {character.Name} for {MagicDamage} damage!");
-				
-				// call the target's defend method
-				// (acts as attacking the target)
-				character.Defend(this, MagicDamage);
+				// generate a random number to determine the damage modifier
+				int rdmMagicModifier = classRandom.Next(-8, 16);
+					
+				// announce the attack and cast the spell
+				if (classRandom.Next(1, 51) > 25)
+				{
+					CastFireball(character, rdmMagicModifier);
+				}
+				else
+				{
+					CastLightningBolt(character, rdmMagicModifier);
+				}
 			}
 			else
 			{
@@ -54,7 +66,64 @@
 			
 			// check if the health is less than or equal to 0
 			// if so, call the "Die" method to announce the death of the character
-			if (Health <= 0) Die();
+			if (Health <= 0)
+			{
+				Die();
+			}
+			else if (classRandom.Next(1, 101) > 75)
+			{
+				// 25%-ish chance to heal self after taking damage
+				CastHeal(this);
+			}
+		}
+
+		/// <summary>
+		/// Function for casting the "Fireball" spell.
+		/// Announces the spell and the damage dealt.
+		/// </summary>
+		/// <param name="target">The target to cast towards.</param>
+		/// <param name="damageModifier">The damage modifier to add to the damage.</param>
+		private void CastFireball(Character target, int damageModifier = 0)
+		{
+			if (Health <= 0) return;
+			
+			Console.WriteLine($"{Name} casted \"Fireball\" towards {target.Name}!");
+			target.Defend(this, MagicDamage + damageModifier);
+		}
+		
+		/// <summary>
+		/// Function for casting the "Lightning-Bolt" spell.
+		/// Announces the spell and the damage dealt.
+		/// </summary>
+		/// <param name="target">The target to cast towards.</param>
+		/// <param name="damageModifier">The damage modifier to add to the damage.</param>
+		private void CastLightningBolt(Character target, int damageModifier = 0)
+		{
+			if (Health <= 0) return;
+			
+			Console.WriteLine($"{Name} casted \"Lightning-Bolt\" upon {target.Name}!");
+			target.Defend(this, MagicDamage + damageModifier);
+		}
+		
+		/// <summary>
+		/// Function for casting the "Heal" spell.
+		/// Announces the spell and the health healed.
+		/// Cannot heal other characters.
+		/// </summary>
+		/// <param name="target">The target to heal.</param>
+		private void CastHeal(Character target)
+		{
+			if (Health <= 0) return;
+			
+			if (target == this)
+			{
+				Console.WriteLine("The mage healed themselves for 10 health points!");
+				Health += 10;
+			}
+			else
+			{
+				Console.WriteLine($"{Name} cannot heal {target.Name}!");
+			}
 		}
 	}
 }
